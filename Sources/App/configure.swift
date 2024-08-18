20d7572b71ecae8eeaa6ae8f40fa440cf7ecb850
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-08-18 17:12:24 +0200

    idk

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,12 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    //app.http.server.configuration.port = 8081
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        username: Environment.get("DATABASE_USERNAME") ?? "name",
+        password: Environment.get("DATABASE_PASSWORD") ?? "pw",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
